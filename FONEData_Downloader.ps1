20b6c701e4878a151ab8f35ddb42e6baf85f6c58
Melissa <>
2025-08-28 13:51:13 -0700

    Update downloader script using manifest

--- FONEData_Downloader.ps1
+++ FONEData_Downloader.ps1
@@ -6,7 +6,7 @@
 $license            = "your_license"
 
 # Release version
-$release_version    = "latest"  
+$release_version    = "latest"
 
 # Target directory - where you want to download files to; The default is current directory.
 $target_directory   = Get-Location
@@ -14,13 +14,13 @@ $target_directory   = Get-Location
 # Program directory - where you put MelissaUpdater.exe; The default is current directory.
 # Melissa Updater source code: https://github.com/MelissaData/MelissaUpdater
 # Melissa Updater binary download link: https://releases.melissadata.net/Download/Library/WINDOWS/NET/ANY/latest/MelissaUpdater.exe
-$program_directory  = "$target_directory\MelissaUpdater" 
-$program_path       = "$program_directory\MelissaUpdater.exe" 
+$program_directory  = "$target_directory\MelissaUpdater"
+$program_path       = "$program_directory\MelissaUpdater.exe"
 
 
 
 ##################### Functions #######################
- 
+
 function Get-File {
     param (
       [parameter(Mandatory=$true)][String]$Filename,
@@ -31,17 +31,17 @@ function Get-File {
       [parameter(Mandatory=$true)][String]$TargetDirectory,
       [parameter(Mandatory=$false)][String]$WorkingDirectory
     )
-       
-    $path = "$target_directory\$TargetDirectory" 
-    
+
+    $path = "$target_directory\$TargetDirectory"
+
     $verifyPath = "$target_directory\$TargetDirectory\$Filename"
     $params = "verify", "-p", "$verifyPath"
     & $program_path $params
-    
+
     if($? -eq $False) {
-      Invoke-Expression "$program_path file -f -n `"$Filename`" -r `"$release_version`" -l `"$license`" -y `"$Type`" -o `"$OperatingSystem`" -c `"$Compiler`" -a `"$Architecture`" -t `"$path`" " 
+      Invoke-Expression "$program_path file -f -n `"$Filename`" -r `"$release_version`" -l `"$license`" -y `"$Type`" -o `"$OperatingSystem`" -c `"$Compiler`" -a `"$Architecture`" -t `"$path`" "
     } else {
-      Invoke-Expression "$program_path file -n `"$Filename`" -r `"$release_version`" -l `"$license`" -y `"$Type`" -o `"$OperatingSystem`" -c `"$Compiler`" -a `"$Architecture`" -t `"$path`" " 
+      Invoke-Expression "$program_path file -n `"$Filename`" -r `"$release_version`" -l `"$license`" -y `"$Type`" -o `"$OperatingSystem`" -c `"$Compiler`" -a `"$Architecture`" -t `"$path`" "
     }
 }
 
@@ -51,10 +51,10 @@ function Get-Manifest {
       [parameter(Mandatory=$true)][String]$TargetDirectory,
       [parameter(Mandatory=$false)][String]$WorkingDirectory
     )
-       
-    $path = "$target_directory\$TargetDirectory" 
 
-    Invoke-Expression "$program_path manifest -p `"$ProductName`" -r `"$release_version`" -l `"$license`" -t `"$path`" " 
+    $path = "$target_directory\$TargetDirectory"
+
+    Invoke-Expression "$program_path manifest -p `"$ProductName`" -r `"$release_version`" -l `"$license`" -t `"$path`" "
 }
 
 ######################## Main #########################
@@ -65,22 +65,4 @@ function Get-Manifest {
 
 # FONE*Data
 $target_path = "Data"
-Get-File -Filename "CNTY.DAT" -Type "DATA" -OperatingSystem "ANY" -Compiler "ANY" -Architecture "ANY" -TargetDirectory $target_path
-
-Get-File -Filename "CNTY.DBF" -Type "DATA" -OperatingSystem "ANY" -Compiler "ANY" -Architecture "ANY" -TargetDirectory $target_path
-
-Get-File -Filename "FONE.DAT" -Type "DATA" -OperatingSystem "ANY" -Compiler "ANY" -Architecture "ANY" -TargetDirectory $target_path
-
-Get-File -Filename "FONE.DBF" -Type "DATA" -OperatingSystem "ANY" -Compiler "ANY" -Architecture "ANY" -TargetDirectory $target_path
-
-Get-File -Filename "HIST.DAT" -Type "DATA" -OperatingSystem "ANY" -Compiler "ANY" -Architecture "ANY" -TargetDirectory $target_path
-
-Get-File -Filename "HIST.DBF" -Type "DATA" -OperatingSystem "ANY" -Compiler "ANY" -Architecture "ANY" -TargetDirectory $target_path
-
-Get-File -Filename "LATA.DAT" -Type "DATA" -OperatingSystem "ANY" -Compiler "ANY" -Architecture "ANY" -TargetDirectory $target_path
-
-Get-File -Filename "LATA.DBF" -Type "DATA" -OperatingSystem "ANY" -Compiler "ANY" -Architecture "ANY" -TargetDirectory $target_path
-
-Get-File -Filename "MSA.DAT" -Type "DATA" -OperatingSystem "ANY" -Compiler "ANY" -Architecture "ANY" -TargetDirectory $target_path
-
-Get-File -Filename "MSA.DBF" -Type "DATA" -OperatingSystem "ANY" -Compiler "ANY" -Architecture "ANY" -TargetDirectory $target_path
+Get-Manifest -ProductName "fonedata" -TargetDirectory $target_path
